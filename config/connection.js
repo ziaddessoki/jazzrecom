78ec16b4d397c6a6487daab197110ba11c8d14ac
Ziad Eldesouky <Ziad.dessoki@yahoo.com>
2019-12-16 04:28:48 -0500

    BE done, FE working on CSS

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
+    user: "um3845gnwiqjhdyg",
+    password: "vl7akkvir3bx6swu",
+    database: "be2xabc6u6ett62p"
   });
 }
 
